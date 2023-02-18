97920459553323251eb9e14569d3dbd4bbedcf1b
hibeenovul <hibeenovul19@gmail.com>
2023-02-17 16:16:35 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function is void
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
