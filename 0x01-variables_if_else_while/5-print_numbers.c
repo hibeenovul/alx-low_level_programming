ee582f197ba50c11553ace26299a303d901d6f87
hibeenovul <hibeenovul19@gmail.com>
2023-02-19 23:48:01 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function is void
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int  i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	return (0);
+}
