ce89e568f252c91602dbb5212a2b78d98c122c66
hibeenovul <hibeenovul19@gmail.com>
2023-02-21 04:17:32 -0800

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet X10
+ * Return: 0 when successful
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
