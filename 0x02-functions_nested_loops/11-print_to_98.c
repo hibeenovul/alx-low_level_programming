2a67664e9e6f5d071645f323c344193bcff267c5
hibeenovul <hibeenovul19@gmail.com>
2023-02-21 11:02:49 -0800

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print 98
+ *@n: parameter decider
+ * Return:0 when successful
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d, ", n);
+}
