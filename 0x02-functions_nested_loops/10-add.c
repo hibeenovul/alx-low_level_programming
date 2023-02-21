9b906f77eaca670c40ca726a393eb6271e163944
hibeenovul <hibeenovul19@gmail.com>
2023-02-21 10:25:12 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - print the add parameters
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return:0 when the code is successful
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - print the times table
+ *
+ * Return:0 when successful
+ */
+
+void times_table(void)
+{
+
+}
