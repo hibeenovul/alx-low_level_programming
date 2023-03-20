77066e448751463e6768e27447e1dc5ff75e8fbf
hibeenovul <hibeenovul19@gmail.com>
2023-03-20 17:29:57 +0100

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+typedef struct dog dog_t;
+
+
+
+
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
+#endif
