432684ee5ae0ab150a32f162814b8709956c82dc
hibeenovul <hibeenovul19@gmail.com>
2023-04-12 15:48:20 +0100

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,72 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * error_file - function to check error in file
- * @file_from: where the file will be copied from
- * @file_to: where the file will be copied to
+ * error_file - function to checks file error
+ * @file_from: function to move file from
+ * @file_to: function to move file to
  * @argv: arguments vector
- * Return: no return
+ * Return: 0 when successful
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
+
+/**
+ * main - function to check the code
+ * @argc: number of arguments.
+ * @argv: number of arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t ch, wr;
+	char bf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+	ch = 1024;
+	while (ch == 1024)
+	{
+		ch = read(file_from, bf, 1024);
+		if (ch == -1)
+			error_file(-1, 0, argv);
+		wr = write(file_to, bf, ch);
+		if (wr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
+
